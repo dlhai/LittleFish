8ce45c6ec21c18376096a3603bbf9294b844bc7a
dlh <dlhai@163.com>
2017-10-24 23:03:37 +0800

    ss

--- db.py
+++ db.py
@@ -1,3 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./chx.db"
+db = SQLAlchemy(app)
+
 class cLass(db.Model):
     __tablename__ = 'class'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,11 +18,12 @@ class Student(db.Model):
     __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10))
-    cLass = db.Column(db.Integer, db.ForeignKey('cLass.id'))
+    cLass = db.Column(db.Integer, db.ForeignKey('class.id'))
     sage = db.Column(db.Integer)
 
 class enroll(db.Model):
     __tablename__ = 'enroll'
+    id = db.Column(db.Integer, primary_key=True)
     course = db.Column(db.Integer, db.ForeignKey('course.id'))
     student = db.Column(db.Integer, db.ForeignKey('student.id'))
     score = db.Column(db.String(10))
